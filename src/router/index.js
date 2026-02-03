4355005b0c77bd8c53359ec6676d4ea429ae17f3
Peerapat Padtawaro <peerapat.p@houseofdev.tech>
2026-02-03 16:53:41 +0700

    Init proj

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: () => import('../views/About.vue') }
-]
+  { path: "/", name: "Home", component: Home },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("@/views/About.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
